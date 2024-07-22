aa3b20f0ba8b0371904c86ae7477763dd46a810c
Albert Lecomte <albert.lecomte1989@gmail.com>
2024-07-22 13:26:38 +0200

    MAJ CSS

--- view/home.php
+++ view/home.php
@@ -1,6 +1,6 @@
-<h1>BIENVENUE SUR LE FORUM</h1>
+<h1 >BIENVENUE SUR LE FORUM</h1>
 
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
+<p class="home_text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
 
 <p>
     <a href="index.php?ctrl=security&action=login">Se connecter</a>
